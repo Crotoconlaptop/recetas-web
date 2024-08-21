2b0923623fcb0a5287e2a3c65f4f420754877faa
Oscar Giordano <98002522+Crotoconlaptop@users.noreply.github.com>
2024-08-21 13:52:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,245 @@
+const bars = {
+    jackie: [
+        { 
+            name: "POLAR", 
+            ingredients: "1tblspn oats, 50ml greek yogurt, 1 2-jan banana, 100ml soy milk", 
+            preparation: "pour all ingredients in the blender and blend, pour in Highball glass", 
+            image: "images/no-image.jpg" 
+        },
+        { 
+            name: "PEANUT BUTTER BANANA", 
+            ingredients: "1pcs banana, 2tsp peanut butter, 150ml almond milk, 2-jan tbs cinnamon powder", 
+            preparation: "pour all ingredients in the blender and blend, pour in Highball glass", 
+            image: "images/no-image.jpg" 
+        },
+        { 
+            name: "FIRST LADY", 
+            ingredients: "40ml fresh peach blend, 25ml balsamic raspberry shrub, 20ml fresh lemon juice, top with champagne", 
+            preparation: "shake all ingredients and top up with champagne, pour in retro martini glass", 
+            image: "images/first-lady.jpg" 
+        },
+        { 
+            name: "BIG APPLE", 
+            ingredients: "80ml apple juice, 60ml pomegranate juice, 20ml basil cordial, 20ml peach+yuzu", 
+            preparation: "shake all ingredientes and strain in to mini V-shape glass, garnish with dehydrated apple, 3 basil leaves", 
+            image: "images/big-apple.jpg" 
+        },
+        { 
+            name: "THE WALL OF 38", 
+            ingredients: "40ml italian spritz, 10ml amaretti, 20ml rosemary and basil essence, 25ml lemon, top sparkling wine", 
+            preparation: "mix well all ingredients, shake well and pour it in white wine glass with ice cubes, garnish with rosemary spray", 
+            image: "images/the-waal-of-38.jpg" 
+        },
+        { 
+            name: "PARK AVENUE", 
+            ingredients: "25ml passion fruit puree, 40ml spiced cane, 15ml granadine, 15ml lemon, top with sparkling wine", 
+            preparation: "pour all ingredients, shake well, pour in coupe top up with sparkling wine and garnish with a leaf coin", 
+            image: "images/park-avenue.jpg" 
+        },
+        { 
+            name: "SOCIALITE", 
+            ingredients: "30ml amaretti, 30ml peach and jasmine syrup, 20ml lemon juice, 3tbs peach puree, 5 drops foamee", 
+            preparation: "dry shake all the ingredients and strain in to martini classic glass, garnish with 3 edible flowers", 
+            image: "images/socialite.jpg" 
+        },
+        { 
+            name: "ICONIC", 
+            ingredients: "80ml pomegranate juice, 10ml lime juice, 20ml pomegranate molasses, 20ml raspberry puree, 20ml simple syrup, 40ml rosso", 
+            preparation: "shake all ingredients and double starin, pour into copper coupe glass and garnish with pomegranate seeds and mint sprig", 
+            image: "images/iconic.jpg" 
+        },
+        { 
+            name: "BERRY PATCH", 
+            ingredients: "6pcs blueberry, 3pcs raspberry, 3pcs blackberry, 3pcs strawberry, 40ml vanilla syrup, 120ml milk, 60ml fresh cream, 1 scoop of ice", 
+            preparation: "pour all the i ngredients in the blender, blend and pour into red wine glass, garnish with berries", 
+            image: "images/berry-patch.jpg" 
+        },
+        { 
+            name: "MANGO MADNESS", 
+            ingredients: "1pcs mango fruit, 5 cubes pineapple, 150ml coconut milk", 
+            preparation: "put all the ingredients in the blender, blend and pour into highball glass, garnish with pineapple cubes", 
+            image: "images/no-image.jpg" 
+        },
+        { 
+            name: "GREEK HOUSE", 
+            ingredients: "10ml dark cane, 40ml amaretti, 20ml lime sherbit, 10ml thyme honey, 25ml lime juice, 20ml yogurt greek", 
+            preparation: "quick stir all ingredients and pour in copper cup with ice", 
+            image: "images/no-image.jpg" 
+        },
+        { 
+            name: "PINK ELEGANCE", 
+            ingredients: "80ml cranberry juice, 40ml strawberry puree, 20ml hibiscus/lemongrass syrup, top with passion fruit and vanilla foam", 
+            preparation: "pour all the ingredients and stir, strain into diamond coupe, top up with foame, garnish with banbou knot", 
+            image: "images/no-image.jpg" 
+        },
+        { 
+            name: "COOL CARCADE", 
+            ingredients: "150ml water, 1tsp hibiscus tea, 50ml apple juice, 1 sprig of mint", 
+            preparation: "pour hot water over hibiscus petals, let it brew and strain, cool the hibiscus tea and pour in the apple juice, stir, serve in highball glass with ie and garnish with mint sprig", 
+            image: "images/cool-carcade.jpg" 
+        },
+        { 
+            name: "CLASSIC ICED TEA", 
+            ingredients: "1tsp black tea, 1pcs lemon, 1tsp sugar, 1 sprig of mint", 
+            preparation: "bring the water to a boil and add black tea, add sugar and leave for 5 minutes, strain the drink, add lemon juice, a few slices of lemon and mint leaves, cool the tea at room temperature, serve with ice cubes", 
+            image: "images/no-image.jpg" 
+        },
+    ],
+    stregis: [
+        { 
+            name: "GOLDEN SANDS", 
+            ingredients: "Dark Cane 50ML, Strawberry & Basil Cordial 25ML, Lemon 25ML, Mango & Mango Foam Top, Glass: Low Ball, Garnish: Coffee Powder Line", 
+            preparation: "Shake all ingredients and double strain and top with the mango foam", 
+            image: "images/golden.jpg" 
+        },
+        { 
+            name: "AND ALL THAT JAZZ", 
+            ingredients: "Sage & Basil Cordial 35ML, Saline Solution 2 Dash, Orange Blossom Water 4 Dash, Lemon Juice 15ML, Sparkling 0% Alcohol (use Bella Gold) Top, Glass: Wine Glass, Garnish: Yellow Jelly Ball", 
+            preparation: "Build and stir direct into the glass", 
+            image: "images/alljazz.jpg" 
+        },
+        { 
+            name: "IV", 
+            ingredients: "Amaretti (Lyres) 60ML, Orange Juice 40ML, Vainilla-Yuzu 35ML, Foamee 5ML, Glass: Retro Martini, Garnish: 4 Bitters", 
+            preparation: "Shake all the ingredients without ice (dry shake) then strain direct to the glass", 
+            image: "images/4.jpg" 
+        },
+        { 
+            name: "JUST IN THYME", 
+            ingredients: "Peach Thyme Cordial 60ML, Lemon Juice 25ML, Pineapple Juice 80ML, Limoncello (use Lime-Sherbit Syrup) 30ML, American Malt (Lyres) 10ML, Fresh Thyme 3 pcs, Glass: Rock Glass, Garnish: Fresh Thyme & Grilled Pineapple", 
+            preparation: "Muddle the fresh thyme to get the flavor then put all the ingredients except the pineapple juice, shake, serve the pineapple juice in the glass and slowly add the content of the shaker", 
+            image: "images/justinthyme.jpg" 
+        },
+        { 
+            name: "FIFTH AVENUE", 
+            ingredients: "Amaretti (Lyres) 80ML, Lemon Juice 30ML, Coffee Cordial 30ML, Glass: Coupe, Garnish: Lemon Zest", 
+            preparation: "Put all the ingredients to the shaker then shake and strain", 
+            image: "images/5avenue.jpg" 
+        },
+        { 
+            name: "ROYAL RIYADH", 
+            ingredients: "Raspberry Sage Cordial 15ML, Strawberry & Basil Cordial 25ML, Lime Juice 25ML, Vanilla Syroup 10ML, Ginger Ale Top, Glass: Diamond Coupe, Garnish: Raspberry, Strawberry, Sancor Basil", 
+            preparation: "Put all the ingredients to the shaker then shake and double strain", 
+            image: "images/royal.jpg" 
+        },
+        { 
+            name: "VIA", 
+            ingredients: "Nectarine Lyres Gin *60ML*, (use Gin Lyres 30ML Peach Monin Cordial 30ML), Pineapple Juice 80ML, Yuzu-Vanilla Mix 30ML, Champagne (use Bella Gold) Top, Glass: Champagne Flute, Garnish: Fake Orange", 
+            preparation: "Dry Shake all the ingredients then shake again with ice and strain and top the champagne direct into the glass", 
+            image: "images/viaaa.jpg" 
+        },
+        { 
+            name: "OUD ELIXIR", 
+            ingredients: "Fresh Peach Blend 60ML, Cardamom & Dates Puree 60ML, Lime Juice 25ML, Balsamic Raspberry Shrub 20ML, Glass: Gold Mug, Garnish: Banana Leaf, Raspberry", 
+            preparation: "Put all the ingredients to the shaker then shake and double strain", 
+            image: "images/oud.jpg" 
+        },
+        { 
+            name: "REGAL MARY", 
+            ingredients: "Tomato Juice 160ML, Bloody Mix 30ML, Dates reduction 10ML, Cloves & Cardamom reduction 10ML, Himalayan Salt Pinch, Ground Black Pepper Top, Glass: High Ball, Garnish: Kefir Lime, Cherry tomato & Rim of Sumac powder", 
+            preparation: "Put all the ingredients to the shaker then shake and strain", 
+            image: "images/regal.jpg" 
+        },
+        { 
+            name: "LADY ASTOR", 
+            ingredients: "White Grapes 0% alcohol wine (use Bella White) 50ML, Cranberry Syrup Monin 10ML, Peach Syrup 10ML, Lemon Juice 25ML, Glass: Wine Glass, Garnish: Pieces of fruits as a sangria inside", 
+            preparation: "Put all the ingredients direct into the glass, all ingredients should be chilled", 
+            image: "images/astor.jpg" 
+        },
+        { 
+            name: "RED SNAPPER", 
+            ingredients: "Tomato Juice 150ML, Worcestershire sauce 10ML, Lemon Juice 15ML, Tabasco sauce 3 drops, Ground Black Pepper Pinch, Salt Pinch, Celery Juice 10ML, Glass: High Ball, Garnish: Celery stick, Lemon Wedge & Rim of Rock Salt", 
+            preparation: "Put all ingredients to the shaker then shake and double starin", 
+            image: "images/red.jpg" 
+        },
+        { 
+            name: "ENERGIZING SMOOTHIE", 
+            ingredients: "avocado 1/4 piece, pineapple juice 75ml, apple juice 75ml, ginger 12g, garnish avocado wedge", 
+            preparation: "put all the ingredients in the blender, blend and pour into highball glass", 
+            image: "images/no-image.jpg" 
+        },
+        { 
+            name: "BRAIN FOOD", 
+            ingredients: "blueberry 10pcs, spinach harfull, pear 1piece, almond milk 150ml, garnish blueberry on stick", 
+            preparation: "put all the ingredients in the blender, blend and pour into highball glass", 
+            image: "images/no-image.jpg" 
+        },
+        { 
+            name: "BREAKFAST ON THE GO", 
+            ingredients: "oats 4 bar spoon, banana 1pc, cinnamon 1 dash, vanilla 10ml, milk 150ml, garnish torched banana", 
+            preparation: "put all the ingredients in the blender, blend and pour into highball glass", 
+            image: "images/no-image.jpg" 
+        },
+        { 
+            name: "CARROT TUMERIC JUICE", 
+            ingredients: "carrot juice 150ml, tumeric 1 bar spoon, ginger 10g, lemon juice 10ml, garnish body carrot", 
+            preparation: "put all the ingredients in the blender, blend and pour into highball glass", 
+            image: "images/no-image.jpg" 
+        },
+        { 
+            name: "LEMON MINT JUICE", 
+            ingredients: "lemon juice 30ml, mint hardfull, sugar syrup 20ml, water 100ml, garnish mint sprigs fresh lemon wheel", 
+            preparation: "put all the ingredients in the blender, blend and pour into highball glass", 
+            image: "images/no-image.jpg" 
+        },
+    ]
+};
+
+
+function showBarMenu(bar) {
+    document.getElementById("main-menu").classList.add("hidden");
+    document.getElementById("bar-menu").classList.remove("hidden");
+
+    const barTitle = bar === 'jackie' ? "Jackie" : "St. Regis Bar";
+    document.getElementById("bar-title").innerText = barTitle;
+
+    const cocktailsList = document.getElementById("cocktails-list");
+    cocktailsList.innerHTML = "";
+
+    bars[bar].forEach(cocktail => {
+        const li = document.createElement("li");
+        li.innerText = cocktail.name;
+        li.onclick = () => showCocktailDetails(cocktail);
+        cocktailsList.appendChild(li);
+    });
+}
+
+function showCocktailDetails(cocktail) {
+    document.getElementById("bar-menu").classList.add("hidden");
+    document.getElementById("cocktail-details").classList.remove("hidden");
+
+    document.getElementById("cocktail-title").innerText = cocktail.name;
+    document.getElementById("ingredients").innerText = "Ingredients: " + cocktail.ingredients;
+    document.getElementById("preparation").innerText = "Preparation: " + cocktail.preparation;
+
+    // Remover imagen anterior si existe
+    const existingImage = document.getElementById("cocktail-image");
+    if (existingImage) {
+        existingImage.remove();
+    }
+
+    // Crear e insertar la nueva imagen después de la preparación
+    if (cocktail.image) {
+        const imageElement = document.createElement("img");
+        imageElement.src = cocktail.image;
+        imageElement.alt = cocktail.name;
+        imageElement.id = "cocktail-image";
+
+        // Insertar la imagen después del texto de preparación
+        const preparationElement = document.getElementById("preparation");
+        preparationElement.parentNode.insertBefore(imageElement, preparationElement.nextSibling);
+    } else {
+        console.error("Imagen no encontrada para " + cocktail.name);
+    }
+}
+
+
+function backToMain() {
+    document.getElementById("bar-menu").classList.add("hidden");
+    document.getElementById("main-menu").classList.remove("hidden");
+}
+
+function backToBarMenu() {
+    document.getElementById("cocktail-details").classList.add("hidden");
+    document.getElementById("bar-menu").classList.remove("hidden");
+}
